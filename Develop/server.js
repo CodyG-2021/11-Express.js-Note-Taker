a5f605fb41decb20e8bf56781d04eb6927d6e9dc
CodyG <gallagher2021@gmail.com>
2021-04-30 14:57:26 -0600

    Routes were not working added in extra line to get it to work, fixed css not linking

--- Develop/server.js
+++ Develop/server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
-require('./routes/apiRoutes')(app);
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
